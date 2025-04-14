97902de5d00f0033219408c1a175d97a8d2a6b34
alexsjmaia <alexsjmaia@gmail.com>
2025-04-14 08:24:50 -0300

    Struct pizza

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+type Pizza struct {
+	ID    int
+	nome  string
+	preco float64
+}
+
+func main() {
+	// toscana, marguerita, atum com queijo
+	var pizzas = []Pizza{
+		{ID: 1, nome: "Toscana", preco: 49.5},
+		{ID: 2, nome: "Marguerita", preco: 79.5},
+		{ID: 3, nome: "Atum com queijo", preco: 69.5},
+	}
+	fmt.Println(pizzas)
+}
